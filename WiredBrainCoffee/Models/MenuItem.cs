e8bc973eb630ad94fc8a5bdcbe6b0a6015c40f9f
SamSong <ymsonghuan@gmail.com>
2020-03-29 15:16:03 -0400

    add asp-route-id and dynamic item page

--- WiredBrainCoffee/Models/MenuItem.cs
+++ WiredBrainCoffee/Models/MenuItem.cs
@@ -7,6 +7,7 @@ namespace WiredBrainCoffee.Models
 {
     public class MenuItem
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Summary { get; set; }
         public string Description { get; set; }
--- WiredBrainCoffee/Pages/Item.cshtml.cs
+++ WiredBrainCoffee/Pages/Item.cshtml.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
 
 namespace WiredBrainCoffee.Pages
 {
     public class ItemModel : PageModel
     {
-        public string Message { get; set; }
+        public MenuItem Item;
 
         public void OnGet(int id)
         {
-            Message = "The id is " + id;
+            var menuService = new MenuService();
+            Item = menuService.GetMenuItems().FirstOrDefault(x => x.Id == id);
         }
     }
 }
